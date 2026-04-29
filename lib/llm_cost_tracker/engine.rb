f26e2aec473c0ec50e150fc2be870efdb767f6e1
Sergii Khomenko <sergey@mm.st>
2026-04-29 17:46:50 +0300

    Removed useless Rails version compatibility check from engine

--- lib/llm_cost_tracker/engine.rb
+++ lib/llm_cost_tracker/engine.rb
@@ -2,11 +2,8 @@
 
 require "rails"
 require_relative "../llm_cost_tracker"
-require_relative "engine_compatibility"
 require_relative "assets"
 require "rack/files"
-
-LlmCostTracker::EngineCompatibility.check_rails_version!(Rails.version)
 
 module LlmCostTracker
   class Engine < ::Rails::Engine
