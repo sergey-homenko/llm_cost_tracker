87d9d311c93dbaa6e40613299d1ef6f7e64b291d
Sergii Khomenko <sergey@mm.st>
2026-05-14 21:39:41 +0300

    Stream CSV export in 500-row batches while preserving sort

--- app/controllers/llm_cost_tracker/calls_controller.rb
+++ app/controllers/llm_cost_tracker/calls_controller.rb
@@ -6,6 +6,7 @@ require "json"
 module LlmCostTracker
   class CallsController < ApplicationController
     CSV_EXPORT_LIMIT = 10_000
+    CSV_EXPORT_BATCH_SIZE = 500
     CSV_FORMULA_PREFIXES = ["=", "+", "-", "@", "\t", "\r"].freeze
     DEFAULT_ORDER = "tracked_at DESC, id DESC"
 
@@ -23,7 +24,7 @@ module LlmCostTracker
         end
         format.csv do
           response.headers["Cache-Control"] = "no-store"
-          send_data render_csv(ordered_scope.limit(CSV_EXPORT_LIMIT)),
+          send_data render_csv(ordered_scope),
                     type: "text/csv",
                     disposition: %(attachment; filename="llm_calls_#{Time.now.utc.strftime('%Y%m%d_%H%M%S')}.csv")
         end
@@ -55,10 +56,21 @@ module LlmCostTracker
       fields = csv_fields
       CSV.generate do |csv|
         csv << fields.map(&:to_s)
-
-        relation.includes(:tag_records).each do |call|
+        each_export_batch(relation) do |call|
           csv << fields.map { |field| csv_value(field, call) }
         end
+      end
+    end
+
+    def each_export_batch(relation, &)
+      offset = 0
+      while offset < CSV_EXPORT_LIMIT
+        batch_size = [CSV_EXPORT_BATCH_SIZE, CSV_EXPORT_LIMIT - offset].min
+        batch = relation.limit(batch_size).offset(offset).preload(:tag_records).to_a
+        break if batch.empty?
+
+        batch.each(&)
+        offset += batch.size
       end
     end
 
