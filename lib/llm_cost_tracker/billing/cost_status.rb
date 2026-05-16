c326aa1703b064c9443263abfe854893d617c08e
Sergii Khomenko <sergey@mm.st>
2026-05-16 08:11:13 +0300

    Drop class << self wrapper around single CostStatus.call

--- lib/llm_cost_tracker/billing/cost_status.rb
+++ lib/llm_cost_tracker/billing/cost_status.rb
@@ -10,34 +10,32 @@ module LlmCostTracker
       PARTIAL = "partial"
       UNKNOWN = "unknown"
 
-      class << self
-        # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-        def call(token_usage:, usage_source:, token_cost:, service_line_items:, total_cost:,
-                 token_pricing_partial: false)
-          return UNKNOWN if usage_source == :unknown
+      # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      def self.call(token_usage:, usage_source:, token_cost:, service_line_items:, total_cost:,
+                    token_pricing_partial: false)
+        return UNKNOWN if usage_source == :unknown
 
-          token_billable = token_usage.priced_quantities.any? { |_key, quantity| quantity.positive? }
-          service_billable = false
-          service_priced = false
-          service_unpriced = false
-          service_line_items.each do |line_item|
-            next unless line_item.billable?
+        token_billable = token_usage.priced_quantities.any? { |_key, quantity| quantity.positive? }
+        service_billable = false
+        service_priced = false
+        service_unpriced = false
+        service_line_items.each do |line_item|
+          next unless line_item.billable?
 
-            service_billable = true
-            service_priced ||= line_item.priced?
-            service_unpriced ||= line_item.unpriced?
-            break if service_priced && service_unpriced
-          end
-
-          priced = (token_billable && !token_cost.nil?) || service_priced || (!token_billable && !service_billable)
-          unpriced = (token_billable && (token_cost.nil? || token_pricing_partial)) || service_unpriced
-          return UNKNOWN if unpriced && !priced
-          return PARTIAL if unpriced
-
-          total_cost.nil? || total_cost.zero? ? FREE : COMPLETE
+          service_billable = true
+          service_priced ||= line_item.priced?
+          service_unpriced ||= line_item.unpriced?
+          break if service_priced && service_unpriced
         end
-        # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+
+        priced = (token_billable && !token_cost.nil?) || service_priced || (!token_billable && !service_billable)
+        unpriced = (token_billable && (token_cost.nil? || token_pricing_partial)) || service_unpriced
+        return UNKNOWN if unpriced && !priced
+        return PARTIAL if unpriced
+
+        total_cost.nil? || total_cost.zero? ? FREE : COMPLETE
       end
+      # rubocop:enable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
     end
   end
 end
